b2a58163425c82a361b7c17bcf62f02c5cdc2c22
john-smilga <johnsmilga@gmail.com>
2019-12-17 09:32:42 -0800

    starter project updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
 import React from "react";
 import Counter from "./Counter";
-import { INCREASE, DECREASE, RESET } from "./actions";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-// setup initial state
-const defaultState = {
-  count: 0,
-  name: "john"
+const App = () => {
+  return <Counter />;
 };
-// reducer
-function reducer(state = defaultState, action) {
-  switch (action.type) {
-    case DECREASE:
-      return { ...state, count: state.count - 1 };
-    case INCREASE:
-      return { ...state, count: state.count + 1 };
-    case RESET:
-      return { ...state, count: 0 };
-    default:
-      return state;
-  }
-}
-// setup store
-const store = createStore(reducer);
-// console.log(store.getState());
-
-function App() {
-  return (
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  );
-}
 
 export default App;
